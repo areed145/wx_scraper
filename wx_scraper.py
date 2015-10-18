2a95ae756f5d04154b0dc4a97eabe0157633b1b3
areed145 <areed145@gmail.com>
2015-10-18 14:13:58 -0700

    still adjusting
    adjusting

--- wx_scraper.py
+++ wx_scraper.py
@@ -11,27 +11,31 @@ import time
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+url = 'http://api.wunderground.com/weatherstation/WXCurrentObXML.asp?ID=KCABAKER38'
+folder = '/Users/areed145/Dropbox/wx_scraper/'
+resample_int = '2min'
+width = 1/24/60*2 #interval is a day, need to divide by reample int
+
 last_obs_time = None
 
 while 2 > 1:
     
-    url = 'http://api.wunderground.com/weatherstation/WXCurrentObXML.asp?ID=KCABAKER38'
     soup = bs4.BeautifulSoup(urllib.request.urlopen(url))
 
     cur_obs_time = pd.Timestamp(soup.find('observation_time_rfc822').getText()).tz_convert('US/Pacific')
     cur_obs_date = cur_obs_time.strftime('%Y%m%d')
-    pickle = '/Users/areed145/Dropbox/wx_scraper/pickles/pickle_'+cur_obs_date+'.pk1'
-    archive = '/Users/areed145/Dropbox/wx_scraper/archives/archive_'+cur_obs_date+'.csv'
-    plot_obs = '/Users/areed145/Dropbox/wx_scraper/plots/obs_'+cur_obs_date+'.png'
-    
+    pickle = folder+'pickles/pickle_'+cur_obs_date+'.pk1'
+    archive = folder+'archives/archive_'+cur_obs_date+'.csv'
+    plot_obs = folder+'plots/obs_'+cur_obs_date+'.png'
+
     try:
         df_store = pd.read_pickle(pickle)
         last_obs_time = df_store.index.max()
     except:
-        df_store = pd.DataFrame()        
+        df_store = pd.DataFrame()
 
     if cur_obs_time != last_obs_time:
-        
+
         station_id = soup.find('station_id').getText()
         full = soup.find('full').getText()
         neighborhood = soup.find('neighborhood').getText()
@@ -62,93 +66,112 @@ while 2 > 1:
         df['precip_1hr_in'] = soup.find('precip_1hr_in').getText()
         df['precip_1hr_metric'] = soup.find('precip_1hr_metric').getText()
         df['precip_today_in'] = soup.find('precip_today_in').getText()
-        
+
         df = df.convert_objects(convert_numeric=True)
         df['cloud_base'] = (((df['temp_f'] - df['dewpoint_f']) / 4.4) * 1000) + 400
         df['obs_d'] = cur_obs_date
-        
+
         df_store = df_store.append(df)
         df_store.to_pickle(pickle)
         df_store.to_csv(archive)
-        
+
         last_obs_time = cur_obs_time
 
-    obs_plot = df_store
-    
-    obs_plot = obs_plot.resample('1min', how = 'mean')
-    
-    plt.close("all")
-    
-    fig = mpl.pyplot.gcf()
-    fig.set_size_inches(10.5, 16.5)
-    
-    plt.subplot(9, 1, 1)
-    plt.title(station_id+': '+full)
-    plt.plot(obs_plot.index, obs_plot['temp_f'], 'b-')
-    plt.plot(obs_plot.index, obs_plot['dewpoint_f'], 'g-')
-    
-    try:
-        plt.plot(obs_plot.index, obs_plot['heat_index_f'], 'r-')
-        plt.plot(obs_plot.index, obs_plot['wind_chill_f'], 'c-')
-    except:
-        pass
-    
-    plt.ylabel('Temp/Dewpt (F)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 2)
-    plt.plot(obs_plot.index, obs_plot['pressure_in'], 'k-')
-    plt.ylabel('Pres (in)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 3)
-    plt.plot(obs_plot.index, obs_plot['wind_mph'], 'y-')
-    plt.plot(obs_plot.index, obs_plot['wind_gust_mph'], 'r.')
-    plt.ylabel('Wind/Gust (mph)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    obs_windNA = obs_plot[obs_plot.wind_mph == 0]
-    obs_wind00 = obs_plot[(obs_plot.wind_mph > 0) & (obs_plot.wind_mph <= 2)]
-    obs_wind02 = obs_plot[(obs_plot.wind_mph > 2) & (obs_plot.wind_mph <= 5)]
-    obs_wind05 = obs_plot[obs_plot.wind_mph >= 5]   
-    
-    plt.subplot(9, 1, 4)
-    plt.plot(obs_windNA.index, obs_windNA['wind_degrees'], '.', ms=3, color='#9191B5')
-    plt.plot(obs_wind00.index, obs_wind00['wind_degrees'], '.', ms=5, color='#FFCC00')
-    plt.plot(obs_wind02.index, obs_wind02['wind_degrees'], '.', ms=6, color='#FF9900')
-    plt.plot(obs_wind05.index, obs_wind05['wind_degrees'], '.', ms=6, color='#FF0000')
-    plt.ylabel('Wind Dir (deg)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    plt.ylim([0,359])
-    
-    plt.subplot(9, 1, 5)
-    plt.plot(obs_plot.index, obs_plot['cloud_base'], 'c-')
-    plt.ylabel('Min Cloudbase (ft MSL)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 6)
-    plt.plot(obs_plot.index, obs_plot['uv'], 'm-')
-    plt.ylabel('UV')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 7)
-    plt.plot(obs_plot.index, obs_plot['solar_radiation'])
-    plt.ylabel('Solar Rad (w/m^2)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 8)
-    plt.plot(obs_plot.index, obs_plot['relative_humidity'], 'r-')
-    #plt.stackplot(obs_plot.index, obs_plot['relative_humidity'], obs_plot['solar_radiation'])
-    plt.ylabel('Rel Humidity (%)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
-    
-    plt.subplot(9, 1, 9)
-    plt.plot(obs_plot.index, obs_plot['precip_today_in'], 'b-')
-    plt.ylabel('Precip (in)')
-    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+        #plotting
+        obs_plot_n = df_store.resample(resample_int, how = 'mean')
+        obs_plot = df_store.resample(resample_int, how = 'mean').interpolate()        
+        obs_plot_max = df_store.resample(resample_int, how = 'max').interpolate()
+        
+        obs_windNA = obs_plot[obs_plot.wind_mph == 0]
+        obs_wind00 = obs_plot[(obs_plot.wind_mph > 0) & (obs_plot.wind_mph <= 2)]
+        obs_wind02 = obs_plot[(obs_plot.wind_mph > 2) & (obs_plot.wind_mph <= 5)]
+        obs_wind05 = obs_plot[(obs_plot.wind_mph > 5) & (obs_plot.wind_mph <= 10)]
+        obs_wind10 = obs_plot[obs_plot.wind_mph >= 10]
 
-    fig.savefig(plot_obs, dpi=200)
-    
-    print('WU checked: '+str(cur_obs_time))
-    
+        obs_gustNA = obs_plot_max[obs_plot_max.wind_gust_mph == 0]
+        obs_gust00 = obs_plot_max[(obs_plot_max.wind_gust_mph > 0) & (obs_plot_max.wind_gust_mph <= 2)]
+        obs_gust02 = obs_plot_max[(obs_plot_max.wind_gust_mph > 2) & (obs_plot_max.wind_gust_mph <= 5)]
+        obs_gust05 = obs_plot_max[(obs_plot_max.wind_gust_mph > 5) & (obs_plot_max.wind_gust_mph <= 10)]
+        obs_gust10 = obs_plot_max[obs_plot_max.wind_gust_mph >= 10]
+
+        plt.close("all")
+
+        fig = mpl.pyplot.gcf()
+        fig.set_size_inches(10.5, 16.5)
+        plt.suptitle(station_id+': '+full+'\n'+cur_obs_date+', Last Observation: '+last_obs_time.strftime('%H:%M:%S'), fontsize=20)
+        
+        plt.subplot(7, 1, 1)
+        plt.bar(obs_plot.index, obs_plot['cloud_base']/100, width, color='#99CCFF', edgecolor='none')
+        plt.ylabel('Min Cloudbase (ft MSL)')
+        #plt.ylim([0,100])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        plt.subplot(7, 1, 2)
+        plt.plot(obs_plot.index, obs_plot['temp_f'], color='#3366FF',linestyle='-')
+        plt.plot(obs_plot.index, obs_plot['dewpoint_f'], color='#CCFF33',linestyle='-')
+
+        try:
+            plt.plot(obs_plot_n.index, obs_plot_n['heat_index_f'], color='#FF0066',linestyle='-')
+            plt.plot(obs_plot_n.index, obs_plot_n['wind_chill_f'], color='#00FFCC',linestyle='-')
+        except:
+            pass
+
+        plt.ylabel('Temp/Dewpt (F)')
+        #plt.ylim([0,110])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        plt.subplot(7, 1, 3)
+        plt.plot(obs_plot.index, obs_plot['pressure_in'], color='#FF5C33',linestyle='-')
+        plt.ylabel('Pres (in)')
+        #plt.ylim([29.6,30.6])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+#        plt.subplot(8, 1, 4)
+#        plt.plot(obs_plot.index, obs_plot['wind_mph'], 'y-')
+#        plt.plot(obs_plot_max.index, obs_plot_max['wind_gust_mph'], 'r.')
+#        plt.ylabel('Wind/Gust (mph)')
+#        #plt.ylim([0,25])
+#        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        plt.subplot(7, 1, 4)
+        plt.bar(obs_gustNA.index, obs_gustNA['wind_degrees']-obs_gustNA['wind_degrees']+359, width, edgecolor='none', color='#FFFFFF')
+        plt.bar(obs_gust00.index, obs_gust00['wind_degrees']-obs_gust00['wind_degrees']+359, width, edgecolor='none', color='#FFCC00')
+        plt.bar(obs_gust02.index, obs_gust02['wind_degrees']-obs_gust02['wind_degrees']+359, width, edgecolor='none', color='#FF9900')
+        plt.bar(obs_gust05.index, obs_gust05['wind_degrees']-obs_gust05['wind_degrees']+359, width, edgecolor='none', color='#FF0000')
+        plt.bar(obs_gust10.index, obs_gust10['wind_degrees']-obs_gust10['wind_degrees']+359, width, edgecolor='none', color='#FF3385')
+        plt.bar(obs_windNA.index, obs_windNA['wind_degrees']-obs_windNA['wind_degrees']+250, width, edgecolor='none', color='#FFFFFF')
+        plt.bar(obs_wind00.index, obs_wind00['wind_degrees']-obs_wind00['wind_degrees']+250, width, edgecolor='none', color='#FFCC00')
+        plt.bar(obs_wind02.index, obs_wind02['wind_degrees']-obs_wind02['wind_degrees']+250, width, edgecolor='none', color='#FF9900')
+        plt.bar(obs_wind05.index, obs_wind05['wind_degrees']-obs_wind05['wind_degrees']+250, width, edgecolor='none', color='#FF0000')
+        plt.bar(obs_wind10.index, obs_wind10['wind_degrees']-obs_wind10['wind_degrees']+250, width, edgecolor='none', color='#FF3385')
+        plt.plot(obs_plot.index, obs_plot['wind_degrees'], 'k-', ms=2)
+        plt.ylabel('Wind Dir (deg)')
+        plt.ylim([0,359])
+        plt.grid(b=True, which='both', color='k',linestyle='-')        
+
+        plt.subplot(7, 1, 5)
+        plt.bar(obs_plot.index, obs_plot['solar_radiation']/100, width, color='#FFDB4D', edgecolor='none')
+        plt.bar(obs_plot.index, obs_plot['uv'], width, color='#DB4DB8', edgecolor='none')
+        plt.ylabel('Solar Rad (w/mm^2) / UV')
+        plt.ylim([0,10])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        plt.subplot(7, 1, 6)
+        plt.bar(obs_plot.index, obs_plot['relative_humidity'], width, color='#256EB8', edgecolor='none')
+        plt.bar(obs_plot.index, 100 - obs_plot['relative_humidity'], width, bottom=obs_plot['relative_humidity'], color='#B2CCFF', edgecolor='none')
+        plt.ylabel('Rel Humidity (%)')
+        plt.ylim([0,100])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        plt.subplot(7, 1, 7)
+        plt.bar(obs_plot.index, obs_plot['precip_today_in'], width, color='#335CD6', edgecolor='none')
+        plt.plot(obs_plot.index, obs_plot['precip_1hr_in'], color='#00CCFF',linestyle='-')
+        plt.ylabel('Precip (in)')
+        plt.ylim([0,0.5])
+        plt.grid(b=True, which='both', color='k',linestyle='-')
+
+        fig.savefig(plot_obs, dpi=200)
+
+        print('WU checked: '+str(cur_obs_time))
+
     time.sleep(15)
